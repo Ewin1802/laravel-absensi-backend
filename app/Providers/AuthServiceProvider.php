6724b263905112c890331f677acfbea287a49cc9
Ewin1802 <elantapa@gmail.com>
2024-07-18 09:27:46 +0800

    fortify, login, logout

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Fortify\Fortify;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Fortify::loginView(function () {
+            return view('pages.auth.auth-login');
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// use App\Http\Controllers\UserController;
+// use App\Http\Controllers\CompanyController;
+// use App\Http\Controllers\AttendanceController;
+// use App\Http\Controllers\PermissionController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    // Route::resource('users', UserController::class);
+    // Route::resource('companies', CompanyController::class);
+    // Route::resource('attendances', AttendanceController::class);
+    // Route::resource('permissions', PermissionController::class);
 });
+
+
